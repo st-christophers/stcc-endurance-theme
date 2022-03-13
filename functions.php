5155ae7117349c1d26619e3947e13c8b10ca78b1
Tom Spencer <tom.spencer@studiozeffa.com>
2022-03-13 18:27:28 +0000

    Add shortcode for listing child pages

--- functions.php
+++ functions.php
@@ -533,3 +533,25 @@ if(is_admin()){
 	}
 
 }
+
+// List child pages
+if ( ! function_exists( 'endurance_list_child_pages' ) ) {
+	function endurance_list_child_pages() {
+
+		global $post;
+
+		if ( is_page() && $post->post_parent )
+				$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+		else
+				$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+
+		if ( $childpages ) {
+				$string = '<ul class="page_children_list">' . $childpages . '</ul>';
+		}
+
+		return $string;
+
+	}
+
+	add_shortcode('list_child_pages', 'endurance_list_child_pages');
+}
